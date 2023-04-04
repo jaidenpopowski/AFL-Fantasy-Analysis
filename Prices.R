009aa908826b4e06e4241b1b86c6ba1e2f5ce998
Jaiden Popowski <98011675+jaidenpopowski@users.noreply.github.com>
2023-04-04 19:46:10 +1000

    Update Prices.R

--- Prices.R
+++ Prices.R
@@ -1,54 +1,37 @@
 # Fetch AFL Fantasy/SuperCoach player prices from Footywire
-library(fitzRoy) # AFL data
-library(tidyverse) # data manipulation
-library(rvest) # static web scraping
 
-fetch_player_prices <- function(season = year(Sys.Date()), round = c(1:24), positions = c("DE","MI","RU","FO"), game = "dream_team") {
+fetch_player_prices <- function(season = lubridate::year(Sys.Date()), round = c(1:24), game = "dream_team") {
   
-  if(!is.numeric(season)) {season = year(Sys.Date)}
-  
-  if(!is.character(positions)) {positions = c("DE","MI","RU","FO")}
-  
-  if(!is.character(game)) {game = "dream_team"}
-  
-  rounds <- data.frame(Season = rep(season,4*length(round)), Round = rep(c(round),4), Position = c(rep("DE",length(round)),rep("MI",length(round)),rep("RU",length(round)),rep("FO",length(round)))) %>% 
-    filter(Position %in% positions) %>% 
-    mutate(Game = game) %>% 
-    arrange(Season,Round)
-  
-  message(paste("Getting prices from", min(rounds$Season), ifelse(min(rounds$Season)!=max(rounds$Season),paste("to", max(rounds$Season)),""))) # print start message
-  
-  pb <- suppressWarnings(progress_estimated(length(rounds$Season)))  # Create progress bar
-  
-  tables <-
-    purrr::pmap_df(list(rounds$Season,rounds$Round,rounds$Position, rounds$Game), ~ {
-      pb$tick()$print() # progress bar updates
-      fetch_round_prices({..1}, {..2}, {..3}, {..4}) # scrape individual coach URLs
-    })
-  
-  final <- tables %>% 
-    mutate(Score = as.integer(Score),Value = as.double(Value),Season = as.integer(Season),Round = as.integer(Round)) %>% 
-    group_by(Season,Round) %>% 
-    mutate(Rank = rank(-Score,ties.method = "min")) %>% 
-    ungroup() %>% 
-    select(Season,Round,Rank,Player,Team,Position,Salary,Score,Value,Salary_current)
-
-  return(final)
-}
-
-# Function to retrieve the table for an individual coach
-fetch_round_prices <- function(Season,Round,Position,Game) {
-  
-  round_list <- read_html(x = paste0("https://www.footywire.com/afl/footy/",Game,"_round?year=",Season,"&round=",Round,"&p=",Position,"&s=T")) %>% 
-    html_element(xpath = '//table[@width = "688"]')
-  
-  if (class(round_list)!="xml_missing") {
-  return(
-    round_list %>% 
-      html_table() %>% 
-      janitor::row_to_names(row_number = 1) %>% 
-      mutate(Season = Season, Round = Round, Position = Position) %>%
-      select(Season, Round, Rank, Player, Position, Team, Salary_current = 4, Salary = 5, Score = 6, Value = 7)
-    )
+  if(mean(season %in% c(2010:lubridate::year(Sys.Date())))<1) {
+    rlang::abort(glue::glue("{season} is not a valid season. Must be seasons between 2010 and {lubridate::year(Sys.Date())}."))
   }
+  
+  if(mean(round %in% c(1:24))<1) {
+    rlang::abort(glue::glue("{round} is not a valid round. Must be rounds between 1 and 24."))
+  }
+  
+  if(!(game %in% c("dream_team","supercoach"))) {
+    rlang::abort(glue::glue("{game} is not a valid input for game. Should be one of dream_team, supercoach."))
+  }
+
+  message(paste("Getting prices from season/s", min(season), ifelse(min(season)!=max(season),paste("to", max(season)),""))) # print start message
+  
+  purrr::pmap_df(
+    list(game,rep(season,each = length(round)),round), 
+    purrr::possibly(
+      ~ rvest::read_html(x = paste0("https://www.footywire.com/afl/footy/",{..1},"_round?year=",{..2},"&round=",{..3},"&p=&s=T")) |>
+        rvest::html_element(xpath = '//table[@width = "688"]') |> 
+        rvest::html_table() |>
+        janitor::row_to_names(row_number = 1) |> 
+        dplyr::select(Player, Team, Current_Price = 4, Price = 5, Score = 6, Value = 7) |>
+        dplyr::transmute(
+          Season = {..2}, 
+          Round = {..3}, 
+          Player = sub('\n.*','',Player), 
+          Team, 
+          Current_Price = as.numeric(gsub('[$,]', '',Current_Price)), 
+          Price = as.numeric(gsub('[$,]', '',Price)), 
+          Score = as.integer(Score), 
+          Value = as.double(Value)),
+      otherwise = data.frame()))
 }
