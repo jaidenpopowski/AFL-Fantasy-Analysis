9f77de6b19216845cbc4998e1ca7222780e64949
Jaiden Popowski <98011675+jaidenpopowski@users.noreply.github.com>
2023-04-05 17:10:35 +1000

    Add fetch_player_prices()

--- fantasyR/functions.R
+++ fantasyR/functions.R
@@ -1,25 +1,26 @@
 # Web scraping functions for AFL Fantasy and SuperCoach
 
-## use `source("https://raw.githubusercontent.com/jaidenpopowski/AFL-Fantasy-Analysis/main/fantasyR/functions.R")` to load these functions into your R session.
+## use `source("https://raw.githubusercontent.com/jaidenpopowski/AFL-Fantasy-Analysis/main/fantasyR/functions.R")` to load these functions into your R environment
 
-fetch_player_prices <- function(season = lubridate::year(Sys.Date()), round = c(1:24), game = "dream_team") {
+fetch_player_prices_footywire <- function(season = lubridate::year(Sys.Date()), round = c(1:24), game = "dream_team") {
+  # example... fetch_player_prices(season = 2019, round = 1:23, game = "supercoach")
   
   if(mean(season %in% c(2010:lubridate::year(Sys.Date())))<1) {
-    rlang::abort(glue::glue("{season} is not a valid season. Must be seasons between 2010 and {lubridate::year(Sys.Date())}."))
+    rlang::abort(glue::glue("{season[!(season %in% 2010:lubridate::year(Sys.Date()))]} is not a valid season. Must be seasons between 2010 and {lubridate::year(Sys.Date())}."))
   }
   
   if(mean(round %in% c(1:24))<1) {
-    rlang::abort(glue::glue("{round} is not a valid round. Must be rounds between 1 and 24."))
+    rlang::abort(glue::glue("{round[!(round %in% 1:24)]} is not a valid round. Must be rounds between 1 and 24."))
   }
   
   if(!(game %in% c("dream_team","supercoach"))) {
     rlang::abort(glue::glue("{game} is not a valid input for game. Should be one of dream_team, supercoach."))
   }
-
+  
   message(paste("Getting prices from season/s", min(season), ifelse(min(season)!=max(season),paste("to", max(season)),""))) # print start message
   
   purrr::pmap_df(
-    list(game,rep(season,each = length(round)),round), 
+    list(game,rep(season,each = length(round)),rep(round,times = length(season))), 
     purrr::possibly(
       ~ rvest::read_html(x = paste0("https://www.footywire.com/afl/footy/",{..1},"_round?year=",{..2},"&round=",{..3},"&p=&s=T")) |>
         rvest::html_element(xpath = '//table[@width = "688"]') |> 
@@ -30,7 +31,7 @@ fetch_player_prices <- function(season = lubridate::year(Sys.Date()), round = c(
           Season = {..2}, 
           Round = {..3}, 
           Player = sub('\n.*','',Player), 
-          Team, 
+          Team,
           Current_Price = as.numeric(gsub('[$,]', '',Current_Price)), 
           Price = as.numeric(gsub('[$,]', '',Price)), 
           Score = as.integer(Score), 
